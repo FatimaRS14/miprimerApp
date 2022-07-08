ab4278932c9274b0cdab814b3af7a45c0160cbb7
Fatima Rojas <faty.aome.rojas14@gmail.com>
2022-07-08 10:53:10 -0500

    Iniciacion del hello world

--- hello.py
+++ hello.py
@@ -1,8 +1,14 @@
+#Gestion de solicitudes HTTP
+#importacion del objeto flask
+
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__) #se crea la instancia app. Se utiliza para gestionar las solicitudes web entrantes y enviar las respuestas al usuario
+#variable especial __name__ marcara el nombre del modulo donde este guardada 
 
 
-@app.route('/')
+
+@app.route('/') 
 def hello():
+    #Creacion de de funcion que devilvera las respuestas
     return 'Hello, World!'
