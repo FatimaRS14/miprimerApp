685375949919f400337ddc67061d973b55c454a5
Fatima Rojas <faty.aome.rojas14@gmail.com>
2022-07-12 11:29:34 -0500

    entradas

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import sqlite3
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
+
 from werkzeug.exceptions import abort
 
 def get_db_connection(): #Esta funcion sirve para abrir un coneccion de base de datos y ejecucion de la consulta SQL
