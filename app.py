51f6f5ab6b38e0fdabc060e1fab70366766470e3
Fatima <faty.aome.rojas14@gmail.com>
2022-07-11 23:24:11 -0500

    Entradas unicas

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 import sqlite3
-from flask import Flask, render_template
 
-. . .
 from flask import Flask, render_template
+from werkzeug.exceptions import abort
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
 
-. . .
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+
 
 @app.route('/')
 def index():
